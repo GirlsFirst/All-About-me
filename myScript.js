1f68c3a4ae9f6a6e1d7b92eb4c355a3b8048fba3
furley25 <fsierravs25@gmail.com>
2019-07-17 10:43:24 -0500

    first commit

---
+++ myScript.js
@@ -0,0 +1,39 @@
+// alert("Hello World")
+// const name = "Felicity"
+// const number = 2;
+//
+// function timesThree(num) {
+//   return num * 3;
+// };
+//
+// function timesFive(num) {return num * 5 };
+//
+// function helloWorld( { alert("helloWorld") };
+//
+// if (5 = 5) }
+//   alert("yay")
+//
+// let text = ""
+//   for (var i = 0; i < 5; i++) {
+//     text += "Num: " + i;
+//   }
+//
+// let num = 0
+//   for (var i = 0; i < 5; i++) {
+//     num += i
+//   }
+
+}
+//  console.log("Hello World");
+//  debugger;
+//  num = 1;
+//  console.log(num);
+
+// var quantity = 0
+//   ('Grey').click(function)  }
+//
+//   ('Gray').click(function(e){
+// e.preventdefault();
+// var quantity = parseInt($('#quantity').val());
+//     ('#quantity').val(quantity + 1);
+//   }
